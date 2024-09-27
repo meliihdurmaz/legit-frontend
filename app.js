9d499221d8db25d656c28eb5fa78becd7ec4a7e9
melih <durmazmelih.ml@gmail.com>
2024-09-27 14:29:46 +0300

    Refactor app.js to use path module for file paths

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
@@ -9,35 +7,17 @@ const app = express();
 
 app.use(express.static('public'));
 
-const hostname = '127.0.0.1';
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html'); 
+    // 'public/index.html' dosyasını gönder
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// const server = http.createServer((req, res) => {
-//     // İsteğin URL'sine göre farklı içerikler döndür
-//     if (req.url === '/') {
-//         res.statusCode = 200;
-//         res.setHeader('Content-Type', 'text/html');
-//         // HTML içeriğini burada döndürüyoruz
-//         fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
-//             if (err) {
-//                 res.statusCode = 500;
-//                 res.end('Hata: İçerik yüklenemedi.');
-//                 return;
-//             }
-//             res.end(data);
-//         });
-//     } else {
-//         res.statusCode = 404;
-//         res.end('404: Sayı bulunamadı');
-//     }
-// });
 
-app.listen(PORT,hostname, () => {
-    console.log(`Sunucu http://${hostname}:${PORT}/ adresinde çalışıyor`);
+app.listen(PORT, () => {
+    console.log(`Sunucu http://127.0.0.1:${PORT} adresinde çalışıyor`);
 });
 // server.listen(port, hostname, () => {
 //     console.log(`Sunucu http://${hostname}:${port}/ adresinde çalışıyor`);
