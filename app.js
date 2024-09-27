e5f6f0545943bea7f59609a36ca9af92125f4149
melih <durmazmelih.ml@gmail.com>
2024-09-27 15:27:31 +0300

    Refactor app.js to use path module for file paths

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const path = require('path');
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 
+// Bot tokeninizi buraya ekleyin
 const token = '7642700137:AAGL1ptojbliCSLRgzIf0dlLBNd6LCtV368';
 
+// Telegram botunu başlat
 const bot = new TelegramBot(token, { polling: true });
 
-
 const app = express();
-
 app.use(express.static('public'));
-
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,7 +18,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
+// '/start' komutu alındığında
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     const telegramId = msg.from.id; // Kullanıcının Telegram ID'si
@@ -29,22 +28,21 @@ bot.onText(/\/start/, (msg) => {
     const url = `https://telagramapplegit-a55c4719cc06.herokuapp.com/?telegramId=${telegramId}&username=${userName}`;
     
     // Inline buton oluşturma
-    const keyboard = [
-        [InlineKeyboardButton("Doğrulama yap", url)] // Buton URL'sini burada ayarlayın
-    ];
-    const reply_markup = InlineKeyboardMarkup(keyboard);
+    const keyboard = {
+        inline_keyboard: [
+            [{ text: "Doğrulama yap", url: url }] // Buton URL'sini burada ayarlayın
+        ]
+    };
 
     // Kullanıcıya mesaj gönder
     bot.sendMessage(
         chatId,
         'Merhaba! Aşağıdaki butona tıklayarak doğrulama yapabilirsiniz',
-        { reply_markup }
+        { reply_markup: keyboard }
     );
 });
 
+// Sunucuyu dinle
 app.listen(PORT, () => {
     console.log(`Sunucu http://127.0.0.1:${PORT} adresinde çalışıyor`);
 });
-// server.listen(port, hostname, () => {
-//     console.log(`Sunucu http://${hostname}:${port}/ adresinde çalışıyor`);
-// });
