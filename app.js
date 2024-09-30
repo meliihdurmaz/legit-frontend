9d0057187ba666f65ad64d484adde33755df2286
melih <durmazmelih.ml@gmail.com>
2024-09-30 17:22:42 +0300

    Refactor app.js to enable serving static files and update user endpoint route

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 // const port = 3000;
 
 // Gerekli middleware
 app.use(express.static('public'));
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
+app.use(cors({ origin: '*' }));
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// });
 
 app.get('/user', (req, res) => {
-    const { userId } = req.params;
+    // const { userId } = req.params;
     const token = req.query.token; // Token'ı al
 
     // Token doğrulama ve kullanıcı bilgilerini işle
     if (token) {
         // Kullanıcı bilgilerini al ve göster
-        res.send(`<h1>Hoş Geldiniz, Kullanıcı ID: ${userId}</h1><p>Token: ${token}</p>`);
+        res.send(`<h1>Hoş Geldiniz, Kullanıcı ID:}</h1><p>Token: ${token}</p>`);
     } else {
         res.status(403).send('Geçersiz token');
     }
 });
 
-// app.use(cors({ origin: '*' }));
-// app.use(express.static('public'));
-// app.use(express.json());
-// app.use(cookieParser());
+
 
 const PORT = process.env.PORT || 3000;
 
