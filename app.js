d17eff7777fa81da8d2c4150d8aa3aeca5a630e7
melih <durmazmelih.ml@gmail.com>
2024-09-30 18:05:16 +0300

    Refactor app.js to update user endpoint route and token display logic

--- app.js
+++ app.js
@@ -12,19 +12,6 @@ app.use(cors({ origin: '*' }));
 //     res.sendFile(__dirname + '/public/index.html');
 // });
 
-app.get('/user', (req, res) => {
-    // const { userId } = req.params;
-    const token = req.query.token; // Token'ı al
-
-    // Token doğrulama ve kullanıcı bilgilerini işle
-    if (token) {
-        // Kullanıcı bilgilerini al ve göster
-        res.send(`<h1>Hoş Geldiniz, Kullanıcı ID:}</h1><p>Token: ${token}</p>`);
-    } else {
-        res.status(403).send('Geçersiz token');
-    }
-});
-
 
 
 const PORT = process.env.PORT || 3000;
