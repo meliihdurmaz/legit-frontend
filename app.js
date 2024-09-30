c34544d5ef48caee5486f3a223a8fbeb928a5964
melih <durmazmelih.ml@gmail.com>
2024-09-30 13:54:57 +0300

    Refactor app.js to remove commented out code, update service URL in /start handler, and add cookie parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 const cors = require('cors');
 const fetch = require('node-fetch');
+const cookieParser = require('cookie-parser'); // Cookie parser kullanımı
 
 
 
@@ -15,16 +16,50 @@ const app = express();
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000;
+function parseJwt(token) {
+    if (!token) {
+        throw new Error("Geçersiz token: Token boş veya null.");
+    }
 
+    const base64Url = token.split('.')[1];
+
+    if (!base64Url) {
+        throw new Error("Geçersiz token: Payload bulunamadı.");
+    }
+
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(c => {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
 app.get('/', (req, res) => {
     const token = req.query.token;
-    // 'public/index.html' dosyasını gönder
+    print("deneme")
+    print(token);
+
+    console.log(token);
     if (token) {
-        res.cookie('bearerToken', token, { httpOnly: true }); // httpOnly güvenli çerez
+        try {
+            const decodedToken = parseJwt(token); // Token'ı çöz
+            print(decodedToken);
+            console.log('Çözümleme sonucu:', decodedToken); // Çözümleme sonucu
+
+            res.cookie('bearerToken', token, { httpOnly: true }); // httpOnly güvenli çerez
+            res.sendFile(path.join(__dirname, 'public', 'index.html')); // HTML dosyasını gönder
+        } catch (error) {
+            console.error(error.message);
+            return res.status(400).send('Token çözümleme hatası: ' + error.message);
+        }
+    } else {
+        return res.status(401).send('Token gerekli.'); // Hata yanıtı
     }
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // app.get('/twitter/authorizeUrl', async (req, res) => {
