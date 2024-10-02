fe1212820d84bc3aae5f50ad25476206824ec261
melih <durmazmelih.ml@gmail.com>
2024-10-03 00:58:37 +0300

    Refactor app.js to remove unused token variable and update HTML language attribute

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors({ origin: '*' }));
 
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     if (req.method === 'OPTIONS') {
+//       return res.sendStatus(200);
+//     }
+//     next();
+//   });
+  
+
 app.use(express.static('public')); 
 
 app.get('/', (req, res) => {
