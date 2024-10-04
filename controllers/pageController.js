ca37270a8c8cd6b9a4d0d880722d1309c4db1dcc
melih <durmazmelih.ml@gmail.com>
2024-10-04 16:46:21 +0300

    Refactor axios request URL in home.ejs and update pageController.js to use res.json for redirect after successful GET request

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -38,7 +38,7 @@ exports.addTwitterAccount = function (req, res) {
         .then((response) => {
             console.log('GET İsteği Başarılı:', response.data);
             const redirectUrl = response.data[0];
-            window.location.href=redirectUrl;
+            res.json({ redirectUrl });
             // res.redirect(redirectUrl);
         })
         .catch((error) => {
