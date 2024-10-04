0562ff0d9bede0ce6bd45d2e69720ab614e0ac1c
melih <durmazmelih.ml@gmail.com>
2024-10-04 15:39:53 +0300

    Refactor app.js to include pageRouter for routing, update CORS configuration, include axios library for fetching user data from API, and remove unused token variable

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,6 +1,7 @@
 
 exports.getHomePage = function (req, res) {
     const token = req.query.token;
+    console.log('Token:', token);
     res.render('home', {
         title: 'Home',
         token: token
