385bfd5c47f25c77cd069159284d7f7d38c43e1d
melih <durmazmelih.ml@gmail.com>
2024-10-04 15:32:10 +0300

    Refactor app.js to include pageRouter for routing, update CORS configuration, include axios library for fetching user data from API, and remove unused token variable

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,46 +1,8 @@
-// const Category = require("../models/Category");
-// const Course = require("../models/Course");
 
 exports.getHomePage = function (req, res) {
+    const token = req.query.token;
     res.render('home', {
         title: 'Home',
+        token: token
     });
 }
-// exports.getAboutPage = function (req, res) {
-//     res.render('about', {
-//         title: 'About'
-//     });
-// }
-// exports.getCoursesPage = async function (req, res) {
-//     const categories = await Category.find({});
-//     const categorySlug = await req.query.category;
-//     const category = await Category.findOne({slug: categorySlug})
-
-//     let filter = {};
-//     if (categorySlug) {
-//         filter = {category: category._id}
-//     }
-//     const courses = await Course.find(filter);
-//     res.render('courses', {
-//         title: 'Courses',
-//         courses,
-//         categories
-//     });
-// }
-// exports.getContactPage = function (req, res) {
-//     res.render('contact', {
-//         title: 'Contact'
-//     });
-// }
-
-// exports.getRegisterPage = function (req, res) {
-//     res.render('register', {
-//         title: 'Register'
-//     });
-// }
-
-// exports.getLoginPage = (req, res) => {
-//     res.render('login',{
-//         title: 'Login'
-//     });
-// }
